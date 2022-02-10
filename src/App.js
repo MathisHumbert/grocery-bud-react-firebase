90855892a0421d7468e6d302bcd96f0875802027
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-10 13:04:13 +0100

    set up is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>;
+  const [grocery, setGrocery] = useState([]);
+  const onSubmit = () => {};
+  return (
+    <section className='section-center'>
+      <form onSubmit={onSubmit} className='grocery-form'>
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input type='text' className='grocery' placeholder='e.g eggs' />
+          <button type='submit' className='submit-btn'>
+            submit
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <div className='grocery-list'>
+          {grocery.map((item) => {
+            return <List {...item} key={item.id} />;
+          })}
+        </div>
+        {grocery.length > 0 && (
+          <button className='clear-btn'>clear items</button>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default App;
